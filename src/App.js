f5c94aec8aec393d378f0ba96b51a5f177ce3d88
Relsig <Chris@honoredpath.com>
2019-11-08 23:21:29 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormLabel from 'react-bootstrap/FormLabel';
+import FormControl from 'react-bootstrap/FormControl';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const marked = require('marked');
+marked.setOptions({
+  gfm: true,
+  breaks: true
+})
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      markdown: 
+      '# h1 header\n'+
+      '## h2 header\n' +
+      '[I\'m a link!](www.google.com)\n'+
+      '![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")\n' +
+      '\`inline code\`\n' +
+      '\`\`\`\ncode block\n\`\`\`\n' +
+      '1. a list item\n' +
+      '**some bold text**\n' +
+      '>a block quote\n'
+    };
+    this.updateMarkdown = this.updateMarkdown.bind(this);
+  };
+
+  updateMarkdown (props) {
+    this.setState({markdown: props});
+  }
+
+  render() {
+    let {markdown} = this.state;
+    return (
+      <div className="App container">
+      <div>
+        <FormGroup controlId="formControlsTextarea">
+          <FormLabel>
+            Markdown Input
+          </FormLabel>
+          <FormControl id="editor" as="textarea" placeholder="Enter Markdown" value={markdown} onChange={(event) => this.updateMarkdown(event.target.value)}/>
+        </FormGroup>
+      </div>
+      <div>
+        <h1>Markdown Output</h1>
+        <div id="preview" dangerouslySetInnerHTML = {{__html: marked(markdown)}}>
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
